371936d5727959568f10cdba020a0bc791aaf455
zer0 <osom8979@gmail.com>
2016-01-10 10:35:18 +0900

    Add a ffmpeg dependencies.

--- etc/dependencies/ubuntu-14.04.sh
+++ etc/dependencies/ubuntu-14.04.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+## FFmpeg dependency.
+apt-get install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
+apt-get install yasm
+apt-get install libx264-dev
+apt-get install libmp3lame-dev
+apt-get install libopus-dev
+
 ## libGL.so
 apt-get install libgl1-mesa-dri
 
-## Default editor.
+## Vim editor.
 apt-get install vim exuberant-ctags cscope
 
