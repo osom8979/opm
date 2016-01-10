5987445b09cd8830121756d938bc7af193613a7e
zer0 <osom8979@gmail.com>
2016-01-10 10:57:04 +0900

    Update ubuntu depencencies.

--- etc/dependencies/ubuntu-14.04.sh
+++ etc/dependencies/ubuntu-14.04.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ## FFmpeg dependency.
-apt-get install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
+apt-get install autoconf automake build-essential libtool pkg-config texinfo
+apt-get install zlib1g-dev libfreetype6-dev libsdl1.2-dev
+apt-get install libass-dev libtheora-dev libvorbis-dev libva-dev libvdpau-dev
+apt-get install libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev
 apt-get install yasm
 apt-get install libx264-dev
 apt-get install libmp3lame-dev
@@ -12,4 +15,26 @@ apt-get install libgl1-mesa-dri
 
 ## Vim editor.
 apt-get install vim exuberant-ctags cscope
+
+## Graphics.
+apt-get install imagemagick
+apt-get install comix
+apt-get install gimp
+
+## Internet.
+apt-get install firefox
+
+## Themes & Tweaks.
+apt-get install unity-tweak-tool
+apt-get install ibus-hangul
+apt-get install compizconfig-settings-manager
+
+## Accessories.
+apt-get install speedcrunch
+
+## Font.
+apt-get install fonts-nanum
+
+## System.
+apt-get install nautilus-open-terminal
 
