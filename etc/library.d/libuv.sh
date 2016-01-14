23964cefb0b9e34677c3ba660172eb8cd52e8f5e
zer0 <osom8979@gmail.com>
2016-01-14 21:29:54 +0900

    Create a libuv build script.

---
+++ etc/library.d/libuv.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+## Don't remove DEPENDENCIES variable.
+DEPENDENCIES=
+
+if [[ -z $OPM_LOCAL ]]; then
+    echo 'Not defined OPM_LOCAL variable.'
+    exit 1
+fi
+
+if [[ -z $OPM_TMP ]]; then
+    echo 'Not defined OPM_TMP variable.'
+    exit 1
+fi
+
+NAME='libuv-1.8.0'
+URL='https://codeload.github.com/libuv/libuv/tar.gz/v1.8.0'
+MD5='03c6a70f6ce8cf2d7ccf1f851e21599e'
+TEMP_DIR="$OPM_TMP/build"
+DEST_NAME="$NAME.tar.gz"
+WORK_NAME="$NAME"
+ALREADY="$OPM_LOCAL/lib/libuv.a"
+LOG_PATH="$TEMP_DIR/$NAME-`datetime`.log"
+
+function runLinux {
+    code=$?; [[ $code != 0 ]] && exit $code
+    ./autogen.sh
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    ./configure --prefix=$OPM_LOCAL >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make check >> $LOG_PATH
+
+    code=$?; [[ $code != 0 ]] && exit $code
+    make install >> $LOG_PATH
+}
+
+LINUX_FUNC=runLinux
+MACOSX_FUNC=runLinux
+WINDOWS_FUNC=runLinux
+
+. general-build "$NAME" "$URL" "$MD5" \
+    "$TEMP_DIR" "$DEST_NAME" "$WORK_NAME" "$ALREADY" "$LOG_PATH" \
+    "$LINUX_FUNC" "$MACOSX_FUNC" "$WINDOWS_FUNC"
+
