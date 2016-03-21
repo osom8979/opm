c62929bd8da2c526d4e13a400837be76e250a9aa
zer0 <osom8979@gmail.com>
2016-03-21 16:06:13 +0900

    Update libpng build script.

--- etc/library.d/libpng.sh
+++ etc/library.d/libpng.sh
@@ -11,9 +11,9 @@ OPM_TMP=$OPM_HOME/tmp
 ## Don't remove DEPENDENCIES variable.
 DEPENDENCIES=zlib.sh:
 
-NAME='libpng-1.6.20'
-URL='http://jaist.dl.sourceforge.net/project/libpng/libpng16/1.6.20/libpng-1.6.20.tar.gz'
-MD5='53166795d924950988a5513d3e605333'
+NAME='libpng-1.6.21'
+URL='ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.21.tar.gz'
+MD5='aca36ec8e0a3b406a5912243bc243717'
 TEMP_DIR="$OPM_TMP/build"
 DEST_NAME="$NAME.tar.gz"
 WORK_NAME="$NAME"
