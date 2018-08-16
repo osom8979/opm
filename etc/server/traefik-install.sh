e9173762d86ce2999f5ce00ce1b57b658be45f37
zer0 <osom8979@gmail.com>
2018-08-16 10:00:22 +0900

    Update treafik.toml file.

--- etc/server/traefik-install.sh
+++ etc/server/traefik-install.sh
@@ -5,21 +5,18 @@ if [[ $(id -u) -ne 0 ]]; then
     exit 1
 fi
 
-DOMAIN=$1
-EMAIL=$2
-
-if [[ -z $DOMAIN || -z $EMAIL ]]; then
-    echo "Usage: $0 {domain} {email}"
+ACME_DOMAIN=$1
+ACME_EMAIL=$2
+if [[ -z $ACME_DOMAIN || -z $ACME_EMAIL ]]; then
+    echo "Usage: $0 {acme_domain} {acme_email}"
     exit 1
 fi
 
-echo "Domain: $DOMAIN"
-echo "E-mail: $EMAIL"
+echo "ACME Domain: $ACME_DOMAIN"
+echo "ACME E-mail: $ACME_EMAIL"
 
-STACK_NAME=traefik-stack
 NET_NAME=traefik-net
-NET_EXISTS=`docker network ls | grep traefik-net`
-
+NET_EXISTS=`docker network ls | grep $NET_NAME`
 if [[ -z $NET_EXISTS ]]; then
     echo "Create $NET_NAME overlay network"
     docker network create -d overlay "$NET_NAME"
@@ -27,10 +24,19 @@ else
     echo "Exists $NET_NAME"
 fi
 
-OPT_DIR=/opt/traefik
-TOML_DEST="$OPT_DIR/traefik.toml"
-ACME_DEST="$OPT_DIR/acme.json"
+TOML_TEMPLATE=traefik-template.toml
+if [[ ! -f "$TOML_TEMPLATE" ]]; then
+    echo "Not found $TOML_TEMPLATE file"
+    exit 1
+fi
 
+COMPOSE_YML=traefik-compose.yml
+if [[ ! -f "$COMPOSE_YML" ]]; then
+    echo "Not found $COMPOSE_YML file"
+    exit 1
+fi
+
+OPT_DIR=/opt/traefik
 if [[ -d "$OPT_DIR" ]]; then
     echo "Exists $OPT_DIR directory"
 else
@@ -38,34 +44,27 @@ else
     mkdir -p "$OPT_DIR"
 fi
 
-TOML_TEMPLATE=traefik-template.toml
-COMPOSE_NAME=traefik-compose.yml
-
-if [[ ! -f "$TOML_TEMPLATE" ]]; then
-    echo "Not found $TOML_TEMPLATE file"
-    exit 1
-fi
-if [[ ! -f "$COMPOSE_NAME" ]]; then
-    echo "Not found $COMPOSE_NAME file"
-    exit 1
-fi
-
-if [[ -f "$TOML_DEST" ]]; then
-    echo "Exists $TOML_DEST file"
+TOML_PATH="$OPT_DIR/traefik.toml"
+if [[ -f "$TOML_PATH" ]]; then
+    echo "Exists $TOML_PATH file"
 else
-    echo "Create $TOML_DEST file"
-    cat "$TOML_TEMPLATE" | sed -e "s/@DOMAIN_NAME@/$DOMAIN/g" -e "s/@EMAIL@/$EMAIL/g" > "$TOML_DEST"
+    echo "Create $TOML_PATH file"
+    cat "$TOML_TEMPLATE" | sed -e "s/@ACME_DOMAIN@/$ACME_DOMAIN/g" -e "s/@ACME_EMAIL@/$ACME_EMAIL/g" > "$TOML_PATH"
 fi
 
-if [[ -f "$ACME_DEST" ]]; then
-    echo "Exists $ACME_DEST file"
+ACME_PATH="$OPT_DIR/acme.json"
+if [[ -f "$ACME_PATH" ]]; then
+    echo "Exists $ACME_PATH file"
 else
-    echo "Create $ACME_DEST file"
-    touch "$ACME_DEST" # && chmod 600 "$ACME_DEST"
+    echo "Create $ACME_PATH file"
+    ## Error starting provider *acme.Provider: unable to get ACME account:
+    ## permissions 644 for acme.json are too open, please use 600
+    touch "$ACME_PATH" && chmod 600 "$ACME_PATH"
 fi
 
+STACK_NAME=traefik
 echo "Deploy stack: $STACK_NAME"
-docker stack deploy -c "$COMPOSE_NAME" "$STACK_NAME"
+docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
 
-echo 'Done.'
+echo "Done ($?)."
 
