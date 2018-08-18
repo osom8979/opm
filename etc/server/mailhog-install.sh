dd11a9fc5b7b088f5bd05e9ff0efd8c3d200d57e
zer0 <osom8979@gmail.com>
2018-08-18 23:01:19 +0900

    Create MailHog install script.

---
+++ etc/server/mailhog-install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+NET_NAME=mailhog-net
+NET_EXISTS=`docker network ls | grep $NET_NAME`
+if [[ -z $NET_EXISTS ]]; then
+    echo "Create $NET_NAME overlay network"
+    docker network create -d overlay "$NET_NAME"
+else
+    echo "Exists $NET_NAME"
+fi
+
+STACK_NAME=mailhog
+COMPOSE_YML=mailhog-compose.yml
+echo "Deploy stack: $STACK_NAME"
+docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
+
+echo "MailHog web: http://localhost:10002/"
+echo "Local MailHog SMTP: $NET_NAME:1025"
+echo "Local MailHog HTTP: $NET_NAME:8025"
+echo "Done ($?)."
+
