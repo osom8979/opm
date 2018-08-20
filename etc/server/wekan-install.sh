6e78fbe3b7603ee1152a7744454c5b5acabd3bb4
zer0 <osom8979@gmail.com>
2018-08-20 09:04:05 +0900

    Create wekan install script.

---
+++ etc/server/wekan-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FRONTEND_HOST=$1
+if [[ -z $FRONTEND_HOST ]]; then
+    echo "Usage: $0 {frontend_host}"
+    exit 1
+fi
+
+export FRONTEND_HOST
+echo "Frontend Host: $FRONTEND_HOST"
+
+COMPOSE_YML=wekan-compose.yml
+if [[ ! -f "$COMPOSE_YML" ]]; then
+    echo "Not found $COMPOSE_YML file"
+    exit 1
+fi
+
+STACK_NAME=wekan
+echo "Deploy stack: $STACK_NAME"
+docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
+
+echo "Done ($?)."
+
