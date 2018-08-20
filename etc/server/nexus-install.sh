c8d627f2dca17a9d7375875495f28c3a28885198
zer0 <osom8979@gmail.com>
2018-08-20 12:36:51 +0900

    Remove the nexus-traefik labels.

--- etc/server/nexus-install.sh
+++ etc/server/nexus-install.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-FRONTEND_HOST=$1
-if [[ -z $FRONTEND_HOST ]]; then
-    echo "Usage: $0 {frontend_host}"
-    exit 1
-fi
-
-export FRONTEND_HOST
-echo "Frontend Host: $FRONTEND_HOST"
-
 STACK_NAME=nexus
 COMPOSE_YML=nexus-compose.yml
 echo "Deploy stack: $STACK_NAME"
