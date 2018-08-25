de6da11e9eef6b39d4b7e8ce8cb1bc5286a12aca
zer0 <osom8979@gmail.com>
2018-08-25 12:52:43 +0900

    Update segment in nexus script.

--- etc/server/nexus-install.sh
+++ etc/server/nexus-install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+FRONTEND_HOST=$1
+DOCKER_FRONTEND_HOST=$2
+if [[ -z $FRONTEND_HOST || -z $DOCKER_FRONTEND_HOST ]]; then
+    echo "Usage: $0 {frontend_host} {docker_frontend_host}"
+    exit 1
+fi
+
+export FRONTEND_HOST
+export DOCKER_FRONTEND_HOST
+echo "Frontend host: $FRONTEND_HOST"
+echo "Docker frontend host: $DOCKER_FRONTEND_HOST"
+
 STACK_NAME=nexus
 COMPOSE_YML=nexus-compose.yml
 echo "Deploy stack: $STACK_NAME"
