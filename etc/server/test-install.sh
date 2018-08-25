6b0ab609d60437860dd6e5d425fbd44471f2d330
zer0 <osom8979@gmail.com>
2018-08-25 12:50:27 +0900

    Trivial commit.

--- etc/server/test-install.sh
+++ etc/server/test-install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-COMPOSE_YML=test-compose.yml
-if [[ ! -f "$COMPOSE_YML" ]]; then
-    echo "Not found $COMPOSE_YML file"
-    exit 1
-fi
-
 FRONTEND_HOST=$1
 if [[ -z $FRONTEND_HOST ]]; then
     echo "Usage: $0 {FRONTEND_HOST}"
@@ -15,6 +9,7 @@ fi
 export FRONTEND_HOST
 echo "Frontend Host: $FRONTEND_HOST"
 
+COMPOSE_YML=test-compose.yml
 STACK_NAME=test
 echo "Deploy stack: $STACK_NAME"
 docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
