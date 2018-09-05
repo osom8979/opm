3372fc2c2229e42d8960437bd45cee21f87d56d0
zer0 <osom8979@gmail.com>
2018-09-05 10:00:30 +0900

    Enable nexus-repository-apt.

--- etc/server/11-nexus-install.sh
+++ etc/server/11-nexus-install.sh
@@ -1,48 +1,52 @@
 #!/usr/bin/env bash
 
-#DOCKER_IMAGE_NAME=nexus-repository-apt
-#EXISTS_DOCKER_IMAGE=`docker images --all | grep $DOCKER_IMAGE_NAME`
-#if [[ -z $EXISTS_DOCKER_IMAGE ]]; then
-#    SRC="https://github.com/sonatype-nexus-community/nexus-repository-apt"
-#    DEST=nexus-repository-apt
-#    pushd $PWD
-#    if [[ -d "$DEST/.git" ]]; then
-#        cd $DEST
-#        git pull
-#    else
-#        git clone --depth 1 "$SRC" "$DEST"
-#        cd $DEST
-#    fi
-#
-#    docker build -t $DOCKER_IMAGE_NAME:3.11.0 .
-#    DOCKER_BUILD_CODE=$?
-#    if [[ ! $DOCKER_BUILD_CODE -eq 0 ]]; then
-#        echo "Docker build error ($DOCKER_BUILD_CODE)."
-#        exit $DOCKER_BUILD_CODE
-#    fi
-#    popd
-#fi
+DOCKER_IMAGE_NAME=nexus-repository-apt
+DOCKER_IMAGE_TAG=3.11.0
+export DOCKER_IMAGE_NAME
+export DOCKER_IMAGE_TAG
+
+EXISTS_DOCKER_IMAGE=`docker images --all | grep $DOCKER_IMAGE_NAME`
+if [[ -z $EXISTS_DOCKER_IMAGE ]]; then
+    SRC="https://github.com/sonatype-nexus-community/nexus-repository-apt"
+    DEST=nexus-repository-apt
+    pushd $PWD
+    if [[ -d "$DEST/.git" ]]; then
+        cd $DEST
+        git pull
+    else
+        git clone --depth 1 "$SRC" "$DEST"
+        cd $DEST
+    fi
+
+    docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+    DOCKER_BUILD_CODE=$?
+    if [[ ! $DOCKER_BUILD_CODE -eq 0 ]]; then
+        echo "Docker build error ($DOCKER_BUILD_CODE)."
+        exit $DOCKER_BUILD_CODE
+    fi
+    popd
+fi
 
 FRONTEND_HOST=$1
-DOCKER_FRONTEND_HOST=$2
-if [[ -z $FRONTEND_HOST || -z $DOCKER_FRONTEND_HOST ]]; then
-    echo "Usage: $0 {frontend_host} {docker_frontend_host}"
+EXTENSION_PUBLISH_PORT=${2:-5000}
+if [[ -z $FRONTEND_HOST ]]; then
+    echo "Usage: $0 {frontend_host} {extension_publish_port:5000}"
     exit 1
 fi
 
 export FRONTEND_HOST
-export DOCKER_FRONTEND_HOST
+export EXTENSION_PUBLISH_PORT
 echo "Frontend Host: $FRONTEND_HOST"
-echo "Docker Frontend Host: $DOCKER_FRONTEND_HOST"
+echo "Extension publish port: $EXTENSION_PUBLISH_PORT"
 
+TRAEFIK_LOCAL_HTTPS_PORT=8080
 STACK_NAME=nexus
 COMPOSE_YML=11-nexus-compose.yml
 echo "Deploy stack: $STACK_NAME"
 docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
 CODE=$?
 
-echo "Web url: https://${FRONTEND_HOST}:8080"
-echo "Web url: https://${DOCKER_FRONTEND_HOST}:8080"
+echo "Web url: https://${FRONTEND_HOST}:${TRAEFIK_LOCAL_HTTPS_PORT}"
 echo "  Default username: admin"
 echo "  Default password: admin123"
 echo "  Docker extension port: 8082"
