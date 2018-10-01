620dc4f5720725ee30b652f2ecf37952a90e942d
zer0 <osom8979@gmail.com>
2018-10-01 18:38:47 +0900

    Create lldb_wrapper bash script.

---
+++ etc/vim/opvim/debugger/lldb_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import lldb
+
+def init(debugger, command, result, internal_dict):
+    #target = debugger.GetSelectedTarget()
+    #process = target.GetProcess()
+    #thread = process.GetSelectedThread()
+    pass
+
