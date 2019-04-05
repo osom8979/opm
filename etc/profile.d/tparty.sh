cb0d00e8108b868c13aa942f1cea3f53bfe04b0a
zer0 <osom8979@gmail.com>
2019-04-05 15:34:37 +0900

    Update tparty profile.

--- etc/profile.d/tparty.sh
+++ etc/profile.d/tparty.sh
@@ -4,23 +4,34 @@ if [[ -z $TPARTY_PREFIX ]]; then
     TPARTY_PREFIX=/usr/local/tparty
 fi
 
+if [[ ! -d $TPARTY_PREFIX ]]; then
+    # Not found TPARTY_PREFIX directory.
+    # But, not error.
+    return 0
+fi
+
 export TPARTY_PREFIX
 
 TPARTY_BIN=$TPARTY_PREFIX/bin
 TPARTY_INC=$TPARTY_PREFIX/include
 TPARTY_LIB=$TPARTY_PREFIX/lib
+TPARTY_PKG=$TPARTY_LIB/pkgconfig
 
-## GCC setting.
-export PATH="$TPARTY_BIN:$PATH"
-export CPATH="$TPARTY_INC:$CPATH"
-export LIBRARY_PATH="$TPARTY_LIB:$LIBRARY_PATH"
-export DYLD_FALLBACK_LIBRARY_PATH="$TPARTY_LIB:$DYLD_FALLBACK_LIBRARY_PATH"
-
-#export LD_LIBRARY_PATH="$TPARTY_LIB:$LD_LIBRARY_PATH"
-#export DYLD_LIBRARY_PATH="$TPARTY_LIB:$DYLD_LIBRARY_PATH"
-
-## Pkg-config setting.
-export PKG_CONFIG_PATH="$TPARTY_LIB/pkgconfig:$PKG_CONFIG_PATH"
+if [[ -d $TPARTY_BIN ]]; then
+    export PATH="$TPARTY_BIN:$PATH"
+fi
+if [[ -d $TPARTY_INC ]]; then
+    export CPATH="$TPARTY_INC:$CPATH"
+fi
+if [[ -d $TPARTY_LIB ]]; then
+    export LIBRARY_PATH="$TPARTY_LIB:$LIBRARY_PATH"
+    #export LD_LIBRARY_PATH
+    #export DYLD_LIBRARY_PATH
+    #export DYLD_FALLBACK_LIBRARY_PATH
+    if [[ -d $TPARTY_PKG ]]; then
+        export PKG_CONFIG_PATH="$TPARTY_PKG:$PKG_CONFIG_PATH"
+    fi
+fi
 
 echo "[Tparty Environment]"
 echo " CFLAGS=\"-I${TPARTY_INC}\""
