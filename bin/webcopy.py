c4cdb275b7a1211486a27565dedb64e10400513f
zer0 <osom8979@gmail.com>
2019-06-25 14:19:34 +0900

    Update webcopy python script.

--- bin/webcopy.py
+++ bin/webcopy.py
@@ -1,6 +1,9 @@
 import os
 from os.path import expanduser
 
+# pip install selenium
+# pip install BeautifulSoup4
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,7 +13,8 @@ from bs4 import BeautifulSoup
 
 HOME_DIR = expanduser('~')
 
-# https://sites.google.com/a/chromium.org/chromedriver/downloads
+# check the chrome version: chrome://settings/help
+# and download driver: https://sites.google.com/a/chromium.org/chromedriver/downloads
 CHROME_DIRVER_PATH = os.path.join(HOME_DIR, 'Downloads', 'chromedriver')
 
 # http://phantomjs.org/download.html
@@ -20,7 +24,15 @@ WIDTH = 1920
 HEIGHT = 1080
 IMPLICITLY_WAIT_SECONDS = 3
 
-next_url = 'https://blog.naver.com/userid/000000000000'
+# N
+#next_url = 'https://blog.naver.com/userid/000000000000'
+#select_frame = True
+
+# T
+url_base = 'https://userid.tistory.com/category/xxx/yyy.?page='
+url_index = 1
+next_url = url_base + str(url_index)
+select_frame = False
 
 if os.path.isfile(CHROME_DIRVER_PATH):
     driver = webdriver.Chrome(CHROME_DIRVER_PATH)
@@ -32,16 +44,16 @@ else:
 
 driver.set_window_size(WIDTH, HEIGHT)
 driver.implicitly_wait(IMPLICITLY_WAIT_SECONDS)
-
 driver.get(next_url)
 
 #WebDriverWait(driver, 10).until(
 #    EC.presence_of_element_located((By.ID, "postListBody"))
 #)
 
-main_frame = driver.find_element_by_id('mainFrame')
-driver.switch_to.default_content()
-driver.switch_to.frame(main_frame)
+if select_frame:
+    main_frame = driver.find_element_by_id('mainFrame')
+    driver.switch_to.default_content()
+    driver.switch_to.frame(main_frame)
 
 #html = driver.page_source
 #result = driver.save_screenshot(save_image_path)
@@ -49,27 +61,58 @@ driver.switch_to.frame(main_frame)
 #soup = BeautifulSoup(html, 'html.parser')
 #post = soup.find(id='postListBody')
 
-last = 166
-for i in range(0, last):
-    post_title = driver.find_element_by_id('title_1').find_elements_by_tag_name('span')[0].text
-    post_body = driver.find_element_by_id('postListBody').text
+"""
+https://selenium-python.readthedocs.io/locating-elements.html
+4. Locating Elements
+Selenium provides the following methods to locate elements in a page:
+* find_element_by_id
+* find_element_by_name
+* find_element_by_xpath
+* find_element_by_link_text
+* find_element_by_partial_link_text
+* find_element_by_tag_name
+* find_element_by_class_name
+* find_element_by_css_selector
+To find multiple elements (these methods will return a list):
+* find_elements_by_name
+* find_elements_by_xpath
+* find_elements_by_link_text
+* find_elements_by_partial_link_text
+* find_elements_by_tag_name
+* find_elements_by_class_name
+* find_elements_by_css_selector
+"""
 
-    bottom_body = driver.find_element_by_id('postBottomTitleListBody')
-    next_items = bottom_body.find_elements_by_tag_name('tr')
-    next_item_01 = next_items[0]
-    next_item_02 = next_items[1]
-    next_link = next_item_02.find_element_by_partial_link_text('find link text')
-    next_url = next_link.get_attribute('href')
+start = 0
+last = 90
+for i in range(start, last):
+    # N
+    #post_title = driver.find_element_by_id('title_1').find_elements_by_tag_name('span')[0].text
+    #post_body = driver.find_element_by_id('postListBody').text
+    #
+    #bottom_body = driver.find_element_by_id('postBottomTitleListBody')
+    #next_items = bottom_body.find_elements_by_tag_name('tr')
+    #next_item_01 = next_items[0]
+    #next_item_02 = next_items[1]
+    #next_link = next_item_02.find_element_by_partial_link_text('find link text')
+    #next_url = next_link.get_attribute('href')
+
+    # T
+    post_title = driver.find_element_by_id('content-inner').find_element_by_class_name('head').text
+    post_body = driver.find_element_by_id('content-inner').find_element_by_class_name('article').text
+    url_index = url_index + 1
+    next_url = url_base + str(url_index)
 
     print('Title: {}'.format(post_title))
-
     with open(post_title + '.txt', 'w') as f:
         f.write(post_body)
-
     print('[{}/{}] done, next: {}'.format(i, last, next_url))
 
     if i < last:
         driver.get(next_url)
+    else:
+        print('Break!')
+        break
 
 print('Done.')
 
