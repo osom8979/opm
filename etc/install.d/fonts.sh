13c28ccb4687aac3dc5e15bc3f0ecd19e5fab8c0
zer0 <osom8979@gmail.com>
2019-12-24 16:07:12 +0900

    Update install script: fonts

--- etc/install.d/fonts.sh
+++ etc/install.d/fonts.sh
@@ -5,7 +5,9 @@ if [[ -z $OPM_HOME ]]; then
     exit 1
 fi
 
+NERD_FONTS_SOURCE_DIR="$OPM_HOME/etc/font"
 NERD_FONTS_NAME="Droid Sans Mono Nerd Font Complete.otf"
+NERD_FONTS_SOURCE_PATH="$NERD_FONTS_SOURCE_DIR/$NERD_FONTS_NAME"
 NERD_FONTS_URL="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
 
 case "$(uname -s)" in
@@ -27,5 +29,9 @@ if [[ ! -d "$FONTS_INSTALL_DIR" ]]; then
     mkdir -p "$FONTS_INSTALL_DIR"
 fi
 
-cp "$OPM_HOME/etc/font/Droid Sans Mono Nerd Font Complete.otf" "$FONTS_INSTALL_DIR"
+cp "$NERD_FONTS_SOURCE_PATH" "$FONTS_INSTALL_DIR"
+
+if [[ "$(uname -s)" == Linux ]]; then
+    fc-cache -f -v
+fi
 
