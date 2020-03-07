735e3e5eeb57eeead5a0e8f25d7eeb16cae5c718
zer0 <osom8979@gmail.com>
2020-03-07 22:12:17 +0900

    Update dependency script: ubuntu-18.04.sh

--- etc/dependencies/ubuntu-18.04.sh
+++ etc/dependencies/ubuntu-18.04.sh
@@ -2,29 +2,54 @@
 
 ## Update
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 
 ## Themes & Tweaks
 apt-get install -y gnome-tweak-tool
-apt-get install -y gnome-shell-extension-system-monitor
+apt-get install -y gnome-shell-extension-system-monitor \
+    gnome-shell-extension-move-clock
 
 ## Network
-apt-get install -y net-tools
+apt-get install -y net-tools curl git
 
 ## Compiler
-apt-get install -y yasm nasm gfortran git cmake build-essential
+apt-get install -y yasm nasm gfortran cmake build-essential
 
 ## Developer tools
-apt-get install -y valgrind cppcheck doxygen
+# cppcheck
+apt-get install -y valgrind doxygen
 
 ## Vim editor
 apt-get install -y neovim
 apt-get install -y exuberant-ctags cscope
-vim +NeoBundleInstall +qall
+# vim +NeoBundleInstall +qall
 
 ## Graphics
-apt-get install -y imagemagick comix gimp
+# comix gimp
+apt-get install -y imagemagick krita
+
+## Multimedia
+apt-get install -y vlc
 
 ## Utilities
 apt-get install -y speedcrunch
+
+## Indicators
+apt-get install -y indicator-multiload
+
+## Language support
+apt-get install -y language-pack-ko \
+    gnome-getting-started-docs-ko \
+    firefox-locale-ko \
+    gnome-user-docs-ko \
+    hunspell-en-gb \
+    hunspell-ko \
+    language-pack-gnome-ko \
+    ibus-hangul \
+    hunspell-en-au \
+    fonts-noto-cjk-extra \
+    hunspell-en-ca \
+    hunspell-en-za \
+    kde-config-fcitx
+
 
