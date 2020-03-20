c529ee82708ed8826a3a0ce15585da261a9e3113
zer0 <osom8979@gmail.com>
2020-03-20 18:23:29 +0900

    Fixed errors in portus install scripts.

--- etc/swarm/12-portus-install.sh
+++ etc/swarm/12-portus-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 FRONTEND_HOST=$1
-FRONTEND_SCHEMA=${1:-https}
+FRONTEND_SCHEMA=${2:-https}
 if [[ -z $FRONTEND_HOST ]]; then
     echo "Usage: $0 {frontend_host} {schema:https}"
     exit 1
@@ -26,12 +26,16 @@ fi
 
 OPT_CERTS=$OPT_DIR/certs
 OPT_REGISTRY=$OPT_DIR/registry
+OPT_DB=$OPT_DIR/db
 
-if [[ -d "$OPT_CERTS" ]]; then
+if [[ ! -d "$OPT_CERTS" ]]; then
     mkdir -p "$OPT_CERTS"
 fi
-if [[ -d "$OPT_REGISTRY" ]]; then
+if [[ ! -d "$OPT_REGISTRY" ]]; then
     mkdir -p "$OPT_REGISTRY"
+fi
+if [[ ! -d "$OPT_DB" ]]; then
+    mkdir -p "$OPT_DB"
 fi
 
 OPT_CERTS_KEY=$OPT_CERTS/portus.key
@@ -43,6 +47,11 @@ else
         -keyout "$OPT_CERTS_KEY" \
         -out "$OPT_CERTS_CRT"
 fi
+
+PORTUS_MINIO_ID_SECRET_NAME=portus-minio-id
+PORTUS_MINIO_ID_SECRET_VALUE=minio
+echo "Create $PORTUS_MINIO_ID_SECRET_NAME secret"
+echo "$PORTUS_MINIO_ID_SECRET_VALUE" | docker secret create "$PORTUS_MINIO_ID_SECRET_NAME" -
 
 PORTUS_MINIO_PW_SECRET_NAME=portus-minio-pw
 PORTUS_MINIO_PW_SECRET_EXISTS=`docker secret ls | grep $PORTUS_MINIO_PW_SECRET_NAME`
@@ -64,9 +73,10 @@ if [[ -f "$REGISTRY_CONFIG_PATH" ]]; then
     echo "Exists $REGISTRY_CONFIG_PATH file"
 else
     echo "Create $REGISTRY_CONFIG_PATH file"
-    cat "$CONFIG_TEMPLATE" | sed \
+    cat "$REGISTRY_CONFIG_TEMPLATE" | sed \
         -e "s/@FRONTEND_SCHEMA@/$FRONTEND_SCHEMA/g" \
         -e "s/@FRONTEND_HOST@/$FRONTEND_HOST/g" \
+        -e "s/@PORTUS_MINIO_ID_SECRET_VALUE@/$PORTUS_MINIO_ID_SECRET_VALUE/g" \
         -e "s/@PORTUS_MINIO_PW_SECRET_VALUE@/$PORTUS_MINIO_PW_SECRET_VALUE/g" \
         > "$REGISTRY_CONFIG_PATH"
 fi
@@ -80,8 +90,26 @@ else
     cp "$REGISTRY_INIT_SRC" "$REGISTRY_INIT_PATH"
 fi
 
-read -sp "Enter the MinIO volume path: " MINIO_DATA_MOUNT_POINT
-echo -e "\n"
+read -p "Enter the MinIO volume path: " MINIO_DATA_MOUNT_POINT
+export MINIO_DATA_MOUNT_POINT
+
+# Error compose settings:
+#  command: mysqld --character-set-server=utf8 --collation-server=utf8_unicode_ci --init-connect='SET NAMES UTF8;' --innodb-flush-log-at-trx-commit=0
+# Error message:
+#  2020-03-20 05:42:21+00:00 [Note] [Entrypoint]: Entrypoint script for MySQL Server 1:10.4.12+maria~bionic started.
+#  2020-03-20 05:42:21+00:00 [Note] [Entrypoint]: Switching to dedicated user 'mysql'
+#  2020-03-20 05:42:21+00:00 [Note] [Entrypoint]: Entrypoint script for MySQL Server 1:10.4.12+maria~bionic started.
+#  2020-03-20 05:42:21+00:00 [Note] [Entrypoint]: Initializing database files
+#  /usr/sbin/mysqld: Too many arguments (first extra is 'NAMES').
+#  2020-03-20  5:42:22 0 [ERROR] Aborting
+DB_CONFIG_SRC=12-portus-db-config
+DB_CONFIG_PATH="$OPT_DB/portus.cnf"
+if [[ -f "$DB_CONFIG_PATH" ]]; then
+    echo "Exists $DB_CONFIG_PATH file"
+else
+    echo "Create $DB_CONFIG_PATH file"
+    cp "$DB_CONFIG_SRC" "$DB_CONFIG_PATH"
+fi
 
 PORTUS_WEB_KEYBASE_SECRET_NAME=portus-web-keybase
 PORTUS_WEB_KEYBASE_SECRET_EXISTS=`docker secret ls | grep $PORTUS_WEB_KEYBASE_SECRET_NAME`
@@ -113,13 +141,12 @@ else
     echo "Exists $PORTUS_DB_PW_SECRET_NAME"
 fi
 
-
 STACK_NAME=portus
 COMPOSE_YML=12-portus-compose.yml
 echo "Deploy stack: $STACK_NAME"
 docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
 CODE=$?
 
-echo "Go to page $FRONTEND_HOST and continue setting."
+echo "Go to page $FRONTEND_SCHEMA://$FRONTEND_HOST and continue setting."
 echo "Done ($CODE)."
 
