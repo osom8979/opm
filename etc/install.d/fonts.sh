4092fcf23634123c63fc52c62786ffdb2573f37c
zer0 <osom8979@gmail.com>
2020-04-05 11:28:44 +0900

    Check if the font is already installed.

--- etc/install.d/fonts.sh
+++ etc/install.d/fonts.sh
@@ -6,16 +6,19 @@ if [[ -z $OPM_HOME ]]; then
 fi
 
 NERD_FONTS_SOURCE_DIR="$OPM_HOME/etc/font"
-NERD_FONTS_NAME="Droid Sans Mono Nerd Font Complete.otf"
-NERD_FONTS_SOURCE_PATH="$NERD_FONTS_SOURCE_DIR/$NERD_FONTS_NAME"
+NERD_FONTS_TITLE="DroidSansMono Nerd Font"
+NERD_FONTS_FILENAME="Droid Sans Mono Nerd Font Complete.otf"
+NERD_FONTS_SOURCE_PATH="$NERD_FONTS_SOURCE_DIR/$NERD_FONTS_FILENAME"
 NERD_FONTS_URL="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
 
 case "$(uname -s)" in
 Darwin)
     FONTS_INSTALL_DIR="$HOME/Library/Fonts"
+    FONTS_INSTALLED=0
     ;;
 Linux)
     FONTS_INSTALL_DIR="$HOME/.local/share/fonts"
+    FONTS_INSTALLED=`fc-list | grep "$NERD_FONTS_TITLE" | wc -l`
     ;;
 CYGWIN*|MINGW*|MSYS*)
     exit 1
@@ -25,13 +28,15 @@ CYGWIN*|MINGW*|MSYS*)
     ;;
 esac
 
-if [[ ! -d "$FONTS_INSTALL_DIR" ]]; then
-    mkdir -p "$FONTS_INSTALL_DIR"
-fi
-
-cp "$NERD_FONTS_SOURCE_PATH" "$FONTS_INSTALL_DIR"
-
-if [[ "$(uname -s)" == Linux ]]; then
-    fc-cache -f -v
+if [[ $FONTS_INSTALLED -eq 0 ]]; then
+    if [[ ! -d "$FONTS_INSTALL_DIR" ]]; then
+        mkdir -p "$FONTS_INSTALL_DIR"
+    fi
+    cp "$NERD_FONTS_SOURCE_PATH" "$FONTS_INSTALL_DIR"
+    if [[ "$(uname -s)" == Linux ]]; then
+        fc-cache -f -v
+    fi
+else
+    echo 'Fonts are already installed.'
 fi
 
