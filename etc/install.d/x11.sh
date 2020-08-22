210b7a80620c99767a7e2733199a300f27327931
zer0 <osom8979@gmail.com>
2020-08-23 08:50:24 +0900

    Create install script: x11

---
+++ etc/install.d/x11.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ -z $OPM_HOME ]]; then
+    print_error 'Not defined OPM_HOME variable.'
+    exit 1
+fi
+if [[ $VIA_INSTALLATION_SCRIPT -ne 1 ]]; then
+    print_error 'You have to do it through the installation script.'
+    exit 1
+fi
+
+AUTOMATIC_YES_FLAG=${AUTOMATIC_YES_FLAG:-0}
+
+XINITRC_CONFIG=$HOME/.xinitrc
+SRC_XINITRC_CONFIG=$OPM_HOME/etc/x11/xinitrc
+
+## Backup the previous x11 config file.
+backup_file "$XINITRC_CONFIG"
+
+## Remove the previous x11 config file.
+rm -f "$XINITRC_CONFIG"
+
+## Install source config.
+echo -e "source $SRC_XINITRC_CONFIG" >> $XINITRC_CONFIG
+print_information "Write xinitrc source config."
+
+
