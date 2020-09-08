174efa20139d53971c0a065eab8f3bf3553225c6
zer0 <osom8979@gmail.com>
2020-09-08 21:26:30 +0900

    Refactoring vim install script.

--- etc/install.d/vim.sh
+++ etc/install.d/vim.sh
@@ -9,8 +9,52 @@ if [[ $VIA_INSTALLATION_SCRIPT -ne 1 ]]; then
     exit 1
 fi
 
+WHICH_NVIM=`which nvim 2> /dev/null`
+WHICH_VIM=`which vim 2> /dev/null`
+WHICH_VI=`which vi 2> /dev/null`
+
+if [[ -n "$WHICH_NVIM" ]]; then
+    VIM_CMD=$WHICH_NVIM
+elif [[ -n "$WHICH_VIM" ]]; then
+    VIM_CMD=$WHICH_VIM
+elif [[ -n "$WHICH_VI" ]]; then
+    VIM_CMD=$WHICH_VI
+else
+    print_error 'Not found vim executable.'
+fi
+
 AUTOMATIC_YES_FLAG=${AUTOMATIC_YES_FLAG:-0}
 INSTALL_VARIABLE_VIM_BUNDLES=$INSTALL_VARIABLE_VIM_BUNDLES
+
+VIMRC=$HOME/.vimrc
+VIM_DIR=$HOME/.vim
+IDEAVIMRC=$HOME/.ideavimrc
+SRC_VIMRC=$OPM_HOME/etc/vim/vimrc
+SRC_IDEAVIMRC=$OPM_HOME/etc/vim/ideavimrc
+NEOVIM_HOME=$HOME/.config/nvim
+NEOVIMRC=$NEOVIM_HOME/init.vim
+BUNDLE_DIR=$HOME/.vim/bundle
+NEOBUNDLE_DIR=$BUNDLE_DIR/neobundle.vim
+NEOBUNDLE_URL=https://github.com/Shougo/neobundle.vim
+
+mkdirs "$BUNDLE_DIR"
+mkdirs "$NEOVIM_HOME"
+
+backup_file "$VIMRC"
+backup_file "$IDEAVIMRC"
+backup_file "$NEOVIMRC"
+
+## Install NeoBundle.vim plugin.
+if [[ -d "$NEOBUNDLE_DIR/.git" ]]; then
+    print_warning "Exists $NEOBUNDLE_DIR"
+else
+    WHICH_GIT=`which git 2> /dev/null`
+    if [[ -n "$WHICH_GIT" ]]; then
+        "$WHICH_GIT" clone "$NEOBUNDLE_URL" "$NEOBUNDLE_DIR"
+    else
+        print_warning 'Not found git executable.'
+    fi
+fi
 
 function install_vimrc
 {
@@ -25,30 +69,6 @@ function install_vimrc
     print_information "Install vimrc: $install_file"
 }
 
-VIMRC=$HOME/.vimrc
-VIM_DIR=$HOME/.vim
-IDEAVIMRC=$HOME/.ideavimrc
-SRC_VIMRC=$OPM_HOME/etc/vim/vimrc
-SRC_IDEAVIMRC=$OPM_HOME/etc/vim/ideavimrc
-NEOVIM_HOME=$HOME/.config/nvim
-NEOVIMRC=$NEOVIM_HOME/init.vim
-BUNDLE_DIR=$HOME/.vim/bundle
-NEOBUNDLE_DIR=$BUNDLE_DIR/neobundle.vim
-
-mkdirs "$BUNDLE_DIR"
-mkdirs "$NEOVIM_HOME"
-
-backup_file "$VIMRC"
-backup_file "$IDEAVIMRC"
-backup_file "$NEOVIMRC"
-
-## Install NeoBundle.vim plugin.
-if [[ -d "$NEOBUNDLE_DIR/.git" ]]; then
-    print_warning "Exists $NEOBUNDLE_DIR"
-else
-    git clone "https://github.com/Shougo/neobundle.vim" "$NEOBUNDLE_DIR"
-fi
-
 install_vimrc "$SRC_VIMRC"     "$VIMRC"
 install_vimrc "$SRC_IDEAVIMRC" "$IDEAVIMRC"
 install_vimrc "$SRC_VIMRC"     "$NEOVIMRC"
@@ -56,6 +76,6 @@ install_vimrc "$SRC_VIMRC"     "$NEOVIMRC"
 ## Install bundles.
 yes_or_no_question "Install bundles?" INSTALL_VARIABLE_VIM_BUNDLES
 if [[ $INSTALL_VARIABLE_VIM_BUNDLES -eq 1 ]]; then
-    vim +NeoBundleInstall +qall
+    "$VIM_CMD" +NeoBundleInstall +qall
 fi
 
