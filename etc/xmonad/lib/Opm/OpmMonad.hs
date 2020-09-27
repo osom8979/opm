6d6fa8991c6b8c9f22d433d69ead1aeab94dea9c
zer0 <osom8979@gmail.com>
2020-09-27 23:28:21 +0900

    Enable XMonad.Hooks.ManageDocks

--- etc/xmonad/lib/Opm/OpmMonad.hs
+++ etc/xmonad/lib/Opm/OpmMonad.hs
@@ -3,14 +3,29 @@ module Opm.OpmMonad (getDefaultOpmMonadSettings, runOpmMonad) where
 import System.Environment
 
 import XMonad
+import XMonad.Hooks.ManageDocks(docks, avoidStruts)
 import XMonad.Util.Run(spawnPipe)
 
 
 opmTerminal = "alacritty"
 
-getDefaultOpmMonadSettings = def {
+opmLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
+    where
+        -- default tiling algorithm partitions the screen into two panes
+        tiled = Tall nmaster delta ratio
+
+        -- The default number of windows in the master pane
+        nmaster = 1
+
+        -- Default proportion of screen occupied by master pane
+        ratio = 1/2
+
+        -- Percent of screen to increment by when resizing panes
+        delta = 3/100
+
+getDefaultOpmMonadSettings = docks def {
         -- simple stuff
-        terminal              = opmTerminal
+        terminal              = opmTerminal,
         -- focusFollowsMouse  = myFocusFollowsMouse,
         -- clickJustFocuses   = myClickJustFocuses,
         -- borderWidth        = myBorderWidth,
@@ -24,7 +39,7 @@ getDefaultOpmMonadSettings = def {
         -- mouseBindings      = myMouseBindings,
 
         -- hooks, layouts
-        -- layoutHook         = myLayout,
+        layoutHook            = opmLayout
         -- manageHook         = myManageHook,
         -- handleEventHook    = myEventHook,
         -- logHook            = myLogHook,
