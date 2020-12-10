e38cdd967aab62fba4a1f96e8a135522fa5ed4b0
zer0 <osom8979@gmail.com>
2020-12-10 10:05:35 +0900

    Fixed errors in nginx-forward install scripts.

--- etc/swarm/51-nginx-forward.sh
+++ etc/swarm/51-nginx-forward.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-FRONTEND_HOST=$1
-BACKEND_HOST=$2
+FRONTEND_HOST=$1   # e.g. test.site.com
+BACKEND_HOST=$2    # e.g. 192.168.0.2:8080
 if [[ -z $FRONTEND_HOST || -z $BACKEND_HOST ]]; then
     echo "Usage: $0 {frontend_host} {backend_host}"
     exit 1
 fi
 
 export FRONTEND_HOST
+export BACKEND_HOST
 echo "Frontend Host: $FRONTEND_HOST"
 echo "Backend Host: $BACKEND_HOST"
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo 'Please run as root.'
+    exit 1
+fi
 
 OPT_DIR=/opt/opm/nginx-forward
 if [[ -d "$OPT_DIR" ]]; then
@@ -21,6 +27,7 @@ fi
 
 DEFAULT_CONFIG_TEMPLATE=51-nginx-forward.conf
 DEFAULT_CONFIG_PATH="$OPT_DIR/$FRONTEND_HOST.conf"
+export DEFAULT_CONFIG_PATH
 if [[ -f "$DEFAULT_CONFIG_PATH" ]]; then
     echo "Exists $DEFAULT_CONFIG_PATH file"
 else
@@ -32,12 +39,14 @@ else
         > "$DEFAULT_CONFIG_PATH"
 fi
 
-STACK_NAME=nginx-forward-$FRONTEND_HOST
+STACK_SUFFIX=`echo $FRONTEND_HOST | sed -e 's/[.]/-/g'`
+STACK_NAME=nginx-forward-$STACK_SUFFIX
 COMPOSE_YML=51-nginx-forward.yml
 echo "Deploy stack: $STACK_NAME"
 docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
 CODE=$?
 
 echo "Create Forward-proxy server: Public(${FRONTEND_HOST}) -> Private(${BACKEND_HOST})"
+echo "Stack name: $STACK_NAME"
 echo "Done ($CODE)."
 
