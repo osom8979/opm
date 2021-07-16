6c21adea17853a81b134bfbd4c0eac9b43818098
zer0 <osom8979@gmail.com>
2021-07-16 11:10:08 +0900

    Create alacritty install script.

---
+++ etc/alacritty/install-dependencies-arch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pacman -S cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon
+
---
+++ etc/alacritty/install-dependencies-debian.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
