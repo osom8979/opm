5185d6d88b80835283697e2e41154a7ede717f80
zer0 <osom8979@gmail.com>
2022-05-13 17:49:37 +0900

    Move swarm config: 51-labelstudio -> 53-labelstudio

--- etc/swarm/53-labelstudio-install.sh
+++ etc/swarm/53-labelstudio-install.sh
@@ -9,6 +9,12 @@ fi
 export FRONTEND_HOST
 echo "Frontend Host: $FRONTEND_HOST"
 
+read -p "Label studio first username: " LABEL_STUDIO_USERNAME
+export LABEL_STUDIO_USERNAME
+
+read -p "Label studio first password: " LABEL_STUDIO_PASSWORD
+export LABEL_STUDIO_PASSWORD
+
 read -p "Enter the database ID: " DB_ID
 export DB_ID
 
@@ -16,7 +22,7 @@ read -p "Enter the database PW: " DB_PW
 export DB_PW
 
 STACK_NAME=labelstudio
-COMPOSE_YML=51-labelstudio-compose.yml
+COMPOSE_YML=53-labelstudio-compose.yml
 echo "Deploy stack: $STACK_NAME"
 docker stack deploy -c "$COMPOSE_YML" "$STACK_NAME"
 CODE=$?
