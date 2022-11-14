1713a31b43df997126d3b4ef1bb033e5c0b5a487
zer0 <osom8979@gmail.com>
2022-11-14 13:59:35 +0900

    Upgrade PS1

--- profile.sh
+++ profile.sh
@@ -1,19 +1,96 @@
 #!/usr/bin/env bash
 
-PROFILE_SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")"; echo "$PWD"`
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
 
 if [[ -z $OPM_HOME ]]; then
-# Not found OPM_HOME variable.
-export OPM_HOME=$PROFILE_SCRIPT_DIR
+export OPM_HOME="$ROOT_DIR"
 fi
 
 ## Default shell configurations.
-
 export PATH=$OPM_HOME/bin:$PATH
 
-CURRENT_SHELL=${SHELL##*/}
-if [[ "$CURRENT_SHELL" == "bash" ]]; then
-export PS1="\e[1m[\t]\e[0m \e[1m\e[32m\u@\h\e[0m:\e[1m\e[96m\w\e[0m\n\\$ "
+# Select Graphic Rendition
+function sgr
+{
+    echo -n "\e[${1}m"
+}
+
+function sgr_reset
+{
+    sgr 0
+}
+
+function sgr_bold
+{
+    sgr 1
+}
+
+function sgr_fg_black
+{
+    sgr 30
+}
+
+function sgr_fg_green
+{
+    sgr 32
+}
+
+function sgr_fg_bright_magenta
+{
+    sgr 95
+}
+
+function sgr_fg_bright_cyan
+{
+    sgr 96
+}
+
+function sgr_bg_bright_yellow
+{
+    sgr 103
+}
+
+function ps_timestamp
+{
+    echo -n "$(sgr_bold)[\t]$(sgr_reset)"
+}
+
+function ps_user_host
+{
+    echo -n "$(sgr_bold)$(sgr_fg_green)\u@\h$(sgr_reset)"
+}
+
+function ps_path
+{
+    echo -n "$(sgr_bold)$(sgr_fg_bright_cyan)\w$(sgr_reset)"
+}
+
+function ps_newline
+{
+    echo -n "\n"
+}
+
+function ps_has_root
+{
+    echo -n "\\$"
+}
+
+function git_branch_name
+{
+    local name
+    name=$(git symbolic-ref --short HEAD 2> /dev/null)
+    if [[ $? -eq 0 ]]; then
+        echo -n "(${name})"
+    fi
+}
+
+function ps_git_branch_name
+{
+    echo -n "$(sgr_bold)$(sgr_fg_bright_magenta)\$(git_branch_name)$(sgr_reset)"
+}
+
+if [[ "${SHELL##*/}" == "bash" ]]; then
+export PS1="$(ps_timestamp) $(ps_user_host):$(ps_path) $(ps_git_branch_name)$(ps_newline)$(ps_has_root) "
 fi
 
 if [[ -z $CLICOLOR ]]; then
