5bbe71b2d4be01aa78b3d547d891faf7eaf0069d
zer0 <osom8979@gmail.com>
2023-02-28 23:32:05 +0900

    Fix syntax error in 201-nvim-remote-plugins.sh

--- etc/setup/ubuntu/201-nvim-remote-plugins.sh
+++ etc/setup/ubuntu/201-nvim-remote-plugins.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-if ! command -v git &> /dev/null; then
-    echo "not found git executable" 1>&2
+if ! command -v nvim &> /dev/null; then
+    echo "not found nvim executable" 1>&2
     exit 1
 fi
 
 
 ARGS=(
     -u "$HOME/.config/nvim/init.vim"
-    -c "try | UpdateRemotePlugins! | finally | qall! | endtry"
+    -c "try | UpdateRemotePlugins | finally | qall! | endtry"
     -V1
     -es
 )
 
-nvim "${ARGS[@]}" 2>&1
+nvim "${ARGS[@]}" 2>&1 | grep -v "^not found in"
 echo -n -e "\n"
