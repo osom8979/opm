78660fac31fd7e0d9faac24b36d1ba702ae0902b
zer0 <osom8979@gmail.com>
2023-03-01 23:14:31 +0900

    Implement setup script: 302-hostname.sh

--- etc/setup.d/ubuntu/302-hostname.sh
+++ etc/setup.d/ubuntu/302-hostname.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run as root" 1>&2
+    exit 1
+fi
+
+if ! command -v hostname &> /dev/null; then
+    echo "Not found hostname command" 1>&2
+    exit 1
+fi
+
 if ! command -v hostnamectl &> /dev/null; then
     echo "Not found hostnamectl command" 1>&2
     exit 1
 fi
 
-sudo hostnamectl set-hostname jumpto
+if ! CURRENT_HOSTNAME=$(hostname); then
+    echo "An unknown error occurred on 'hostname' command" 1>&2
+    exit 1
+fi
+
+echo "The current hostname is '$CURRENT_HOSTNAME'"
+read -r -p "Please enter the hostname to change: " NEXT_HOSTNAME
+
+if ! hostnamectl set-hostname "$NEXT_HOSTNAME"; then
+    echo "Hostname change failed" 1>&2
+    exit 1
+fi
