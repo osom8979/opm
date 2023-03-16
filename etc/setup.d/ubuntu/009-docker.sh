504369858618088bbdb36db78332daf9bd61c0f2
zer0 <osom8979@gmail.com>
2023-03-16 23:07:05 +0900

    Implement setup script: 009-docker.sh

---
+++ etc/setup.d/ubuntu/009-docker.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run as root" 1>&2
+    exit 1
+fi
+
+if ! command -v apt-get &> /dev/null; then
+    echo "Not found apt-get command" 1>&2
+    exit 1
+fi
+
+apt-get remove -y \
+    docker \
+    docker-engine \
+    docker.io \
+    containerd \
+    runc
+apt-get update -y
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+KEYRINGS_DIR=/etc/apt/keyrings
+if [[ ! -d "$KEYRINGS_DIR" ]]; then
+    mkdir -vp "$KEYRINGS_DIR"
+    chmod 0755 "$KEYRINGS_DIR"
+fi
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
+
+if [[ -n $SUDO_USER ]]; then
+    usermod -aG docker "$SUDO_USER"
+fi
+
+echo "Group result: $(grep docker /etc/group)"
