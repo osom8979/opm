06bf726fef13e58ff5a582337c7c702c4d84fe8d
zer0 <osom8979@gmail.com>
2023-07-20 11:30:21 +0900

    Create bin: opy-opencv-videoplayer

---
+++ lib/python3/opencv_video_player.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import cv2
+
+
+def main(src: str, title="VideoPlayer") -> None:
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cap = cv2.VideoCapture(src)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Display the resulting frame
+        cv2.imshow(title, frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyWindow(title)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
