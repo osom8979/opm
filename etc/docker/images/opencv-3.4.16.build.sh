6d7ce24efef31a8b0b3be42595d24bce77458106
zer0 <osom8979@gmail.com>
2023-10-22 00:11:57 +0900

    Fix docker image tag: openv -> opencv

--- etc/docker/images/opencv-3.4.16.build.sh
+++ etc/docker/images/opencv-3.4.16.build.sh
@@ -3,6 +3,6 @@
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
 
 FILE="$ROOT_DIR/opencv-3.4.16.dockerfile"
-TAG="openv:3.4.16"
+TAG="opencv:3.4.16"
 
 docker build -f "$FILE" -t "$TAG" "$ROOT_DIR"
