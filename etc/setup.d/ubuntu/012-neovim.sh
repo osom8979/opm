f96cc7b0d1e1781e094f31a58e92bb92574cb5e8
zer0 <osom8979@gmail.com>
2024-04-09 16:55:53 +0900

    Trivial commit

--- etc/setup.d/ubuntu/012-neovim.sh
+++ etc/setup.d/ubuntu/012-neovim.sh
@@ -14,7 +14,7 @@ NVIM_PATH=/usr/local/bin/nvim
 NVIM_VERSION=v0.9.5
 URL="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim.appimage"
 
-curl -v -L -k -o "$NVIM_PATH" "$URL"
+curl -L -k -o "$NVIM_PATH" "$URL"
 
 if [[ -f "$NVIM_PATH" && ! -x "$NVIM_PATH" ]]; then
     chmod -v +x "$NVIM_PATH"
