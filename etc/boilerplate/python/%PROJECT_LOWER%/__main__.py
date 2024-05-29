716f2dea9000b14dc96689f2edded5d85a1113e3
zer0 <osom8979@gmail.com>
2024-05-29 13:39:47 +0900

    Remove etc/boilerplate

--- etc/boilerplate/python/%PROJECT_LOWER%/__main__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from sys import exit as sys_exit
-
-from %PROJECT_LOWER%.entrypoint import main
-
-if __name__ == "__main__":
-    sys_exit(main())
--- etc/boilerplate/python/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)
-
-cd "$ROOT_DIR" && "$ROOT_DIR/python" setup.py bdist_wheel
